b8b85eb24ea0d6e4169eac0497d93b2b68b89764
Godones <1925466036@qq.com>
2024-08-06 20:23:06 +0800

    add riscv64 support

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -14,3 +14,9 @@ pub use x86::*;
 mod aarch64;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
+
+#[cfg(target_arch = "riscv64")]
+mod riscv64;
+
+#[cfg(target_arch = "riscv64")]
+pub use riscv64::*;
